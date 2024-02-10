37a9f10bc6c16304f5a069aceb8c5676482d66bc
Gabriel Pezo Rojas <156217567+gabrielpezo@users.noreply.github.com>
2024-02-10 00:48:29 +0000

    domaingenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log();
 };
+
+let pronoun = ["the", "our", "her", "his", "their", "your"];
+let adj = ["great", "big", "ourstanding", "amazing", "bad", "ugly"];
+let noun = ["jogger", "racoon", "station", "dress", "hardware", "lightning"];
+let domain = [".com", ".net", ".es", ".cl", ".us", ".io"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let h = 0; h < adj.length; h++) {
+    for (let g = 0; g < noun.length; g++) {
+      for (let l = 0; l < domain.length; l++) {
+        console.log(pronoun[i] + adj[h] + noun[g] + domain[l]);
+      }
+    }
+  }
+}
